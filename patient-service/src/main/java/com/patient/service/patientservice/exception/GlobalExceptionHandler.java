4650190be191574228ef4ade2756485c4602fbc2
Ahmed babay <ahmed1dwar@gmail.com>
2025-08-27 13:17:45 +0200

    refactoring

--- patient-service/src/main/java/com/patient/service/patientservice/exception/GlobalExceptionHandler.java
+++ patient-service/src/main/java/com/patient/service/patientservice/exception/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
-package com.patient.service.patientservice.controller;
+package com.patient.service.patientservice.exception;
 
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
