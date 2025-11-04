105301d1a185007fd388cb203b24af5b7c65fe8a
Ahmed babay <ahmed1dwar@gmail.com>
2025-11-04 19:38:04 +0100

    improved the not found exception

--- patient-service/src/main/java/com/patient/service/patientservice/exception/GlobalExceptionHandler.java
+++ patient-service/src/main/java/com/patient/service/patientservice/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.patient.service.patientservice.exception;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -39,7 +40,7 @@ public class GlobalExceptionHandler {
         log.warn("Patient not found {}", ex.getMessage());
         HashMap<String,String> errors = new HashMap<>();
         errors.put("message","Patient not found");
-        return ResponseEntity.badRequest().body(errors);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errors);
     }
 
 }
